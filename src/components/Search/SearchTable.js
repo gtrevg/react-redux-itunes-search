6093aa6b2b156364167b190d05260741a536017e
Aaron Planell <aaronplanell@gmail.com>
2018-03-11 09:12:42 +0100

    Add sortable columns and review responsive layout.

--- src/components/Search/SearchTable.js
+++ src/components/Search/SearchTable.js
@@ -14,24 +14,42 @@ const getDuration = text => {
 	return minutes.padStart(2, '0') + ':' + seconds.padStart(2, '0');
 };
 
+const sort = (itemA, itemB) => {
+	if (itemA > itemB) return 1;
+	if (itemA < itemB) return -1;
+	return 0;
+};
+
+const getPriceWithoutUnits = priceWithUnits => {
+	const priceWithoutUnitsAsText = priceWithUnits.substr(
+		0,
+		priceWithUnits.indexOf(' ')
+	);
+	const priceWithoutUnits = parseFloat(priceWithoutUnitsAsText);
+	return priceWithoutUnits;
+};
+
 const columns = [
 	{
 		title: 'Title',
 		dataIndex: 'title',
 		key: 'title',
 		className: 'SearchTableTitle',
+		sorter: (a, b) => sort(a.title, b.title),
 	},
 	{
 		title: 'Artist',
 		dataIndex: 'artist',
 		key: 'artist',
 		className: 'SearchTableArtist',
+		sorter: (a, b) => sort(a.artist, b.artist),
 	},
 	{
 		title: 'Album title',
 		dataIndex: 'album',
 		key: 'album',
 		className: 'SearchTableAlbum',
+		sorter: (a, b) => sort(a.album, b.album),
 	},
 	{
 		title: 'Release date',
@@ -39,6 +57,7 @@ const columns = [
 		key: 'release',
 		className: 'SearchTableRelease',
 		render: getDate,
+		sorter: (a, b) => sort(a.release, b.release),
 	},
 	{
 		title: 'Cover',
@@ -55,18 +74,22 @@ const columns = [
 		key: 'lenght',
 		className: 'SearchTableLenght',
 		render: getDuration,
+		sorter: (a, b) => sort(a.lenght, b.lenght),
 	},
 	{
 		title: 'Genre',
 		dataIndex: 'genre',
 		key: 'genre',
 		className: 'SearchTableGenre',
+		sorter: (a, b) => sort(a.genre, b.genre),
 	},
 	{
 		title: 'Price',
 		dataIndex: 'price',
 		key: 'price',
 		className: 'SearchTablePrice',
+		sorter: (a, b) =>
+			sort(getPriceWithoutUnits(a.price), getPriceWithoutUnits(b.price)),
 	},
 ];
 
